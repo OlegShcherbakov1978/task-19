c94a4d1e0e0f5d8a6b47337c2dce62ca8f987947
Oleg <olmag@inbox.ru>
2023-01-21 16:05:44 +0300

    task19

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿begin:
+
+System.Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if (number.Length != 5)
+{
+    System.Console.WriteLine("Неверный ввод");
+    goto begin;
+}
+
+else if (number[0] == number[4] && number[1] == number[3])
+{
+    System.Console.WriteLine("Это число - палиндром");
+}
+
+else
+{
+    System.Console.WriteLine("Это не палиндром");
+}
